0467a66e6aed9b2cd2616c3f67c95b5f5951695b
Tverdokhlib Yurii <tverdokhlibyurii@ukr.net>
2019-12-27 23:46:55 +0200

    task4

--- Task4.py
+++ Task4.py
@@ -1,1 +1,16 @@
-def Wraps(RL):def Wrapper(YT):YT.__name__ = RL.__name__YT.__doc__ = RL.__doc__return YTreturn Wrapperdef Wrapper(wrap):@Wraps(wrap)def wrapper(a):wrap(a)    return wrapper@Wrapperdef Lab3(a):passprint(Lab3.__name__)print(Lab3.__doc__)
+def Wraps(RL):
+    def Wrapper(YT):
+        YT.__name__ = RL.__name__
+        YT.__doc__ = RL.__doc__
+        return YT
+    return Wrapper
+def Wrapper(wrap):
+    @Wraps(wrap)
+    def wrapper(a):
+        wrap(a)
+    return wrapper
+@Wrapper
+def Lab3(a):
+    pass
+print(Lab3.__name__)
+print(Lab3.__doc__)
